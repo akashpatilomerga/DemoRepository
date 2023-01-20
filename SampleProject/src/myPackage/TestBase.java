c5bae30b2cb47bc9469c9f086a7f2ea82dde83ec
akashpatilomerga <patil8akash@gmail.com>
2023-01-20 16:47:37 +0530

    HomePage task
    Hi Team,
    
    I have completed homepage task. please, review it and approve it.

--- SampleProject/src/myPackage/TestBase.java
+++ SampleProject/src/myPackage/TestBase.java
@@ -5,7 +5,7 @@ public class TestBase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Basic Tasks gets added to Project");
+		System.out.println("Basic Tasks gets added to ");
 	}
 
 }
