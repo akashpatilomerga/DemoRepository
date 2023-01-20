d12d9092f22ed09fe24ef940dc97fae9e3efbf26
akashpatilomerga <patil8akash@gmail.com>
2023-01-20 16:23:13 +0530

    Basic Task
    Hi,
    I have completed basic task.
    
    Thanks!

---
+++ SampleProject/src/myPackage/TestBase.java
@@ -0,0 +1,11 @@
+package myPackage;
+
+public class TestBase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Basic Tasks gets added to Project");
+	}
+
+}
